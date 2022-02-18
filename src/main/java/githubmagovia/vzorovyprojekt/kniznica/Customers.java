4270caa7db03057ff809ca0b474fb4d0a38202d8
ppalo21 <ppavlik48@gmail.com>
2022-02-18 16:03:19 +0100

    Customers - upravene

--- src/main/java/githubmagovia/vzorovyprojekt/kniznica/Customers.java
+++ src/main/java/githubmagovia/vzorovyprojekt/kniznica/Customers.java
@@ -2,19 +2,10 @@ package githubmagovia.vzorovyprojekt.kniznica;
 
 public class Customers {
 
-    private Long id;
     private String firstName;
     private String lastName;
     private String email;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getFirstName() {
         return firstName;
