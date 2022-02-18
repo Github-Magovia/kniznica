769c3e3383ea3b988588a726d6b69485ddba520c
misko <missko111@gmail.com>
2022-02-18 11:16:25 +0100

    Prvý commit (base-project)

---
+++ src/main/java/githubmagovia/vzorovyprojekt/kniznica/KniznicaApplication.java
@@ -0,0 +1,13 @@
+package githubmagovia.vzorovyprojekt.kniznica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KniznicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KniznicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/githubmagovia/vzorovyprojekt/kniznica/KniznicaApplicationTests.java
@@ -0,0 +1,13 @@
+package githubmagovia.vzorovyprojekt.kniznica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KniznicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
