85e87461a6be6eacd648374fffd29206ba0a6136
Simox-CU <riecans47@gmail.com>
2022-02-25 12:01:17 +0100

    BooksService create + BooksController update

--- src/main/java/githubmagovia/vzorovyprojekt/kniznica/BooksController.java
+++ src/main/java/githubmagovia/vzorovyprojekt/kniznica/BooksController.java
@@ -7,57 +7,38 @@ import java.util.List;
 
 @RestController
 public class BooksController {
-    public static List<Book> books;
+    private final BooksService booksService;
 
-    public BooksController() { books = init(); }
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
 
-    private List<Book> init() { return new ArrayList<>(); }
-
-    //Create new book title
+    //Create
     @PostMapping("/api/books")
     public void createBook(@RequestBody Book book) {
-        book.setId(books.size());
-        books.add(book);
+        booksService.createBook(book);
     }
 
-    //List all book titles
+    //Get - all
     @GetMapping("/api/books")
     public List<Book> getBooks(@RequestParam(required = false) String name) {
-        if (name == null) { return books; }
-        List<Book> filteredList = new ArrayList<>();
-        for (Book book : books) {
-            if (book.getName().equals(name)) { filteredList.add(book); }
-        }
-        return filteredList;
+        return booksService.getBooks(name);
     }
 
-    //Get book title by id
+    //Get - by id
     @GetMapping("/api/books/{id}")
-    public Book getBook(@PathVariable Integer id) { return books.get(id); }
+    public Book getBook(@PathVariable Integer id) { return booksService.getBook(id); }
 
-    //Update book title
+    //Update
     @PutMapping("/api/books/{id}")
     public Book updateBook(@PathVariable Integer id, @RequestBody Book book) {
-        Book changed = books.get(id);
-        changed.setName(book.getName());
-        changed.setIsbn(book.getIsbn());
-        changed.setAuthorFirstName(book.getAuthorFirstName());
-        changed.setAuthorLastName(book.getAuthorLastName());
-        changed.setBookCount(book.getBookCount());
-        return changed;
+        return booksService.updateBook(id,book);
     }
 
-    //Delete book title
+    //Delete
     @DeleteMapping("/api/books/{id}")
     public void deleteBook(@PathVariable Integer id) {
-        decrementIds(id);
-        books.remove(id.intValue());
+        booksService.deleteBook(id);
     }
 
-    //Decrement id of every book in the list after the removed book
-    private void decrementIds(int id) {
-        int size = books.size();
-        int bookId = id + 1;
-        while (bookId < size) { books.get(bookId++).decrementId(); }
-    }
 }
