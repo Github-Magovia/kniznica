08840428b16474dc36ac74eb023de14dd9c9709d
misko <missko111@gmail.com>
2022-03-04 17:51:30 +0100

    Fix map datatype

--- src/main/java/githubmagovia/vzorovyprojekt/kniznica/borrowing/BorrowingRepository.java
+++ src/main/java/githubmagovia/vzorovyprojekt/kniznica/borrowing/BorrowingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface BorrowingRepository extends JpaRepository<BorrowingEntity, Integer> {
+public interface BorrowingRepository extends JpaRepository<BorrowingEntity, Long> {
 }
